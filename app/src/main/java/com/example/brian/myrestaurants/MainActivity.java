cddb228f09cf4738f4cdcb575fdf70c8fb2a83ec
Brian Marete <bmarete10@gmail.com>
2017-05-22 16:42:14 +0300

    commit

--- app/src/main/java/com/example/brian/myrestaurants/MainActivity.java
+++ app/src/main/java/com/example/brian/myrestaurants/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.brian.myrestaurants;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private Button mFindRestaurantsButton;
+    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+
+        mFindRestaurantsButton.setOnClickListener(v -> {
+            String location = mLocationEditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+        });
     }
 }

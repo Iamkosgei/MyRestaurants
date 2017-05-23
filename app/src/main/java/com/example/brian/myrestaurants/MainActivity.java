414f5a1e088ee6689783e2e5c6b8d646adf25484
Brian Marete <bmarete10@gmail.com>
2017-05-23 05:21:22 +0300

    Change font of AppName text view

--- app/src/main/java/com/example/brian/myrestaurants/MainActivity.java
+++ app/src/main/java/com/example/brian/myrestaurants/MainActivity.java
@@ -1,18 +1,18 @@
 package com.example.brian.myrestaurants;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindRestaurantsButton;
     private EditText mLocationEditText;
+    private TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +21,11 @@ public class MainActivity extends AppCompatActivity {
 
         mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
         mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+
+        Typeface ostrichBoldFont = Typeface.createFromAsset(getAssets(), "fonts/OstrichSans-Bold.otf");
+        Typeface windsongFont = Typeface.createFromAsset(getAssets(), "fonts/Windsong.ttf");
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+        mAppNameTextView.setTypeface(windsongFont);
 
         mFindRestaurantsButton.setOnClickListener(v -> {
             String location = mLocationEditText.getText().toString();

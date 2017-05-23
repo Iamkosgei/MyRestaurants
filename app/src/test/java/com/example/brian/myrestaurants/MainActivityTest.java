a5ce1e70151525cb0b89dd1c046a4b3a2ac90b71
Brian Marete <bmarete10@gmail.com>
2017-05-23 03:59:56 +0300

    Finish Pre-work

--- app/src/test/java/com/example/brian/myrestaurants/MainActivityTest.java
+++ app/src/test/java/com/example/brian/myrestaurants/MainActivityTest.java
@@ -1,5 +1,6 @@
 package com.example.brian.myrestaurants;
 
+import android.content.Intent;
 import android.os.Build;
 import android.widget.TextView;
 
@@ -8,13 +9,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.annotation.Config;
 import org.robolectric.Robolectric;
-import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.shadows.ShadowActivity;
 
 import static junit.framework.Assert.assertTrue;
 
 
 @Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
-@RunWith(RobolectricGradleTestRunner.class)
+@RunWith(RobolectricTestRunner.class)
 public class MainActivityTest {
 private MainActivity activity;
 
@@ -23,9 +25,20 @@ private MainActivity activity;
         activity = Robolectric.setupActivity(MainActivity.class);
     }
 
-     @Test
+    @Test
     public void validateTextViewContent() {
          TextView appNameTextView = (TextView) activity.findViewById(R.id.appNameTextView);
          assertTrue("MyRestaurants".equals(appNameTextView.getText().toString()));
     }
+
+    @Test
+    public void secondActivityStarted() {
+        activity.findViewById(R.id.findRestaurantsButton).performClick();
+        Intent expectedIntent = new Intent(activity, RestaurantsActivity.class);
+        ShadowActivity shadowActivity = org.robolectric.Shadows.shadowOf(activity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+        assertTrue(actualIntent.filterEquals(expectedIntent));
+    }
+
+
 }
